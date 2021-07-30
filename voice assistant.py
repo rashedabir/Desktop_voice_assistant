d20156b16c97fa7aed2db64b15ec9a073e43f03d
Tomal991 <intesarulhaquey4@gmail.com>
2021-07-30 13:10:45 +0600

    added some commands

--- voice assistant.py
+++ voice assistant.py
@@ -6,13 +6,16 @@ import webbrowser
 import os
 import ctypes
 import smtplib
+import subprocess
+import pyjokes
+
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 
-engine.setProperty('voice', voices[1].id)
-newVoiceRate = 145
-engine.setProperty('rate', newVoiceRate)
+engine.setProperty('voice', voices[0].id)
+
 
 
 def speak(audio):
@@ -29,9 +32,9 @@ def wishMe():
     elif(hour >= 17 and hour < 20):
         speak("Good Evening!")
     else:
-        speak("Good Evening!")
+        speak("Good Night!")
 
-    speak("I am your assistant sir! Please tell me how may I help you?")
+    speak("I am your assistant,Jarvis sir! Please tell me how may I help you?")
 
 
 def takeCommand():
@@ -39,7 +42,7 @@ def takeCommand():
     with sr.Microphone() as source:
         print("Listening...")
         s.pause_threshold = 1
-        audio = s.listen(source)
+        audio = s.listen(source,timeout=1,phrase_time_limit=5)
 
         try:
             print("Recognizing...")
@@ -58,8 +61,8 @@ def sendEmail(to, content):
     server.starttls()
 
     # Enable low security in gmail
-    server.login('abir97bd@gmail.com', 'password')
-    server.sendmail('abir97bd@gmail.com', to, content)
+    server.login('intesarulhaquey4@gmail.com', 'hello.991')
+    server.sendmail('intesarulhaquey4@gmail.com', to, content)
     server.close()
 
 
@@ -67,75 +70,73 @@ if __name__ == "__main__":
     wishMe()
     while True:
         query = takeCommand().lower()
+
         if 'wikipedia' in query:
-            speak("Searching Wikipidia...")
+            speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
             results = wikipedia.summary(query, sentences=2)
-            speak("According to Wikipidea")
+            speak("According to Wikipedia")
+            print(results)
             speak(results)
 
-        elif 'youtube' in query:
-            speak("Oppening Youtube...")
+        elif 'youtube' in query or 'open youtube' in query:
+            speak("Opening Youtube...")
             webbrowser.open("youtube.com")
 
-        elif 'google' in query:
+        elif 'google' in query or 'open google' in query:
             speak("Opening Google...")
             webbrowser.open("google.com")
 
-        elif 'github' in query:
+        elif 'github' in query or 'open github' in query:
             speak("Opening Github...")
             webbrowser.open("github.com")
 
-        elif 'stack overflow' in query:
+        elif 'stack overflow' in query or 'open stack overflow' in query:
             speak("Opening stackoverflow...")
             webbrowser.open("stackoverflow.com")
 
         elif 'play music' in query or 'play songs' in query or 'play song' in query:
-            music_dir = 'E:\\MP3 Songs'
+            music_dir = 'D:\\Music'
             songs = os.listdir(music_dir)
             speak("Playing Music...")
             os.startfile(os.path.join(music_dir, songs[0]))
 
         elif 'play videos' in query or 'play video' in query:
-            video_dir = 'D:\\Video Songs'
+            video_dir = 'D:\\React'
             videos = os.listdir(video_dir)
             speak("Playing Video...")
             os.startfile(os.path.join(video_dir, videos[0]))
 
-        elif 'made you' in query or 'created you' in query or 'developed you' in query:
-            pic_dir = 'F:\\My Pic'
+        elif 'who made you' in query or 'who created you' in query or 'who developed you' in query:
+            pic_dir = 'D:\\My pic'
             pic = os.listdir(pic_dir)
-            webbrowser.open("https://rashed-abir.web.app/")
-            os.startfile(os.path.join(pic_dir, pic[131]))
-            speak("its Rashed Khan")
+            webbrowser.open("http://localhost:3000/")
+            os.startfile(os.path.join(pic_dir, pic[0]))
+            speak("Its Intesarul haque Tomal")
 
-        elif 'the time' in query:
+        elif 'the time' in query or "what is the time" in query:
             time = datetime.datetime.now().strftime("%I:%M %p")
+            print(time)
             speak(f"the time is {time}")
 
         elif 'open code' in query or 'vs code' in query:
-            codePath = 'C:\\Users\\rashe\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe'
-            speak("Openning code...")
+            codePath = "C:\\Users\\tomal\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            speak("Opening Vs code...")
             os.startfile(codePath)
 
         elif 'open postman' in query or 'postman' in query:
-            postmanPath = 'C:\\Users\\rashe\\AppData\\Local\\Postman\\Postman.exe'
-            speak("Openning postman...")
+            postmanPath = "C:\\Users\\tomal\\AppData\\Local\\Postman\\Postman.exe"
+            speak("Opening postman...")
             os.startfile(postmanPath)
-
-        elif 'open popcorn' in query or 'pop corn' in query or 'showtime' in query or 'show time' in query:
-            popcornPath = 'C:\\Program Files (x86)\\Popcorn Time\\PopcornTimeDesktop.exe'
-            speak("Enjoy your time, sir...")
-            os.startfile(popcornPath)
 
         elif 'send a mail' in query:
             try:
                 speak("what should I say?")
-                conntent = takeCommand()
-                print(f"your messege: {conntent}\n")
-                speak("whome should i send")
+                content = takeCommand()
+                print(f"your message: {content}\n")
+                speak("whom should i send")
                 to = input("Mail to: ")
-                sendEmail(to, conntent)
+                sendEmail(to, content)
                 speak("Email has been sent !")
             except Exception as e:
                 # print(e)
@@ -148,7 +149,7 @@ if __name__ == "__main__":
         elif 'fine' in query or "good" in query:
             speak("It's good to know that your fine")
 
-        elif 'friday' in query or 'are you here' in query or 'where are you' in query:
+        elif 'Jarvis' in query or 'are you here' in query or 'where are you' in query:
             speak("i am here! Please tell me how may I help you sir?")
 
         elif "what's your name" in query or "What is your name" in query:
@@ -160,16 +161,16 @@ if __name__ == "__main__":
             webbrowser.open(f"https://www.google.com/search?q={query}")
 
         elif 'facebook' in query or 'open facebook' in query:
-            speak("Openning Facebook")
+            speak("Opening Facebook")
             webbrowser.open("https://www.facebook.com/")
 
-        elif "who i am" in query:
+        elif "who am I" in query:
             speak("If you talk then definitely your human.")
 
         elif "why you came to world" in query:
-            speak("Thanks to Rashed Khan. further It's a secret")
+            speak("Thanks to Intesarul Haque Tomal. further It's a secret")
 
-        elif 'is love' in query:
+        elif 'what is love' in query:
             speak("It is 7th sense that destroy all other senses")
 
         elif 'lock window' in query:
@@ -178,19 +179,37 @@ if __name__ == "__main__":
 
         elif 'today' in query:
             today = datetime.date.today().strftime("%A, %d-%B-%Y")
+            print(today)
             speak(f"today is {today}")
+
 
         elif 'yesterday' in query:
             today = datetime.date.today()
             yesterday = today - datetime.timedelta(days=1)
             y = yesterday.strftime("%A, %d-%B-%Y")
+            print(y)
             speak(f"yesterday was {y}")
+
 
         elif 'tomorrow' in query:
             today = datetime.date.today()
             tomorrow = today + datetime.timedelta(days=1)
             t = tomorrow.strftime("%A, %d-%B-%Y")
-            speak(f"Tomorow is {t}")
+            print(t)
+            speak(f"Tomorrow is {t}")
+
+        elif 'notepad' in query or 'open notepad' in query:
+            codePath = "C:\Windows\system32\\notepad.exe"
+            speak("Opening Notepad...")
+            os.startfile(codePath)
+
+
+        elif "restart" in query:
+            subprocess.call(["shutdown", "/r"])
+
+
+        elif 'joke' in query:
+            speak(pyjokes.get_joke())
 
         elif 'exit' in query or 'bye' in query or 'see you later' in query:
             speak("Thanks for giving me your time sir.")
